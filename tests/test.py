352928cc082bd0e8b40ddc1c7478c9bfeb7c7d78
Aleksei Kostromin <kostrominpav@gmail.com>
2023-11-19 21:53:25 +0400

    add test_search_hootel

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,31 @@
 import time
 
-from selene import browser,have,command
+from selene import browser,have,command,be
 
 
-def test_authorization_picaboo():
-    browser.open("https://pikabu.ru/")
+def test_search_hootel():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open("https://russpass.ru")
 
-    browser.element('[placeholder="Логин"]').type("eltempo")
-    browser.element('[placeholder="Пароль"]').type("HRX-a87-gca-iur")
-    browser.element(".button_success").click()
+    browser.element(".OnboardingModal_onboarding-window__content__aId7h").should(be.visible)
+    browser.element(".OnboardingModal_onboarding-window__content__aId7h .button").click()
 
-    browser.element(".user__info-item .user__nick").should(have.text("eltempo"))
+    browser.element("[placeholder='Поиск']").type("Москва")
+    browser.element("//span[normalize-space(.)='Жилье']").click()
+    browser.element("//button[normalize-space(.)='Найти']").click()
+    browser.element(".filterButton_button__zL1VD").click()
 
 
+    browser.element("//label[@for='children']/..//button[@class='counter__button'][2]").click()
+    browser.element("//div[normalize-space(.)='Возраст ребенка']").click()
+    browser.all("#children-options").element_by(have.exact_text("3 года")).click()
+    browser.element("//button[normalize-space(.)='Найти']").click()
+    result = browser.all("#catalogCardsId")
+    for i in result:
+        i.should(be.visible)
+
+
+    browser.quit()
 
 
