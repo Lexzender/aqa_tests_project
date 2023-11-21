97531c0d0ff44db1498c5e47a26e0677b8e463fe
Aleksei Kostromin <kostrominpav@gmail.com>
2023-11-21 21:33:25 +0400

    add test_check_bonus_display, test_activation_eng_ver

--- tests/test.py
+++ tests/test.py
@@ -65,3 +65,47 @@ def test_fill_support_form():
 
 
 
+def test_check_bonus_display():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open("https://russpass.ru/loyalty")
+    browser.element(".bonuses__header-headline").should(be.visible).should(
+        have.text("Путешествуйте с удовольствием и получайте за это подарки"))
+    browser.element(".bonuses__header-caption").should(be.visible).should(
+        have.text("Копите бонусы и обменивайте их на подарки из нашего каталога"))
+    browser.element(".bonuses__section-header h2").should(be.visible).should(have.text("Как накопить бонусы"))
+    browser.element('//div[@class="bonuses__grid-item"][1]').perform(command.js.scroll_into_view)
+    browser.element('//div[@class="bonuses__grid-item"][1]').should(be.visible).should(
+        have.text("Регистрируйтесь на сайте")).should(
+        have.text("Храните в Профиле все покупки и скидки, добавляйте объекты в Избранное и стройте маршруты"))
+    browser.element('//div[@class="bonuses__grid-item"][2]').should(be.visible).should(
+        have.text("Бронируйте отели, покупайте билеты")).should(
+        have.text("Планируйте путешествие и получайте бонусы за покупки"))
+    browser.element('//div[@class="bonuses__grid-item"][3]').should(be.visible).should(
+        have.text("Покупайте рекомендованные предложения")).should(
+        have.text("Ищите карточки со специальным значком RUSSPASS и получайте повышенные бонусы"))
+    browser.element('//div[@class="bonuses__grid-item"][4]').should(be.visible).should(
+        have.text("Подписывайтесь на рассылку")).should(have.text("Получайте дополнительные бонусы за подписку"))
+    browser.element('//div[@class="bonuses__grid-item"][5]').should(be.visible).should(
+        have.text("Оценивайте отели, места и события")).should(
+        have.text("Делитесь вашими впечатлениями и получайте бонусы"))
+
+    browser.quit()
+
+def test_activation_eng_ver():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open("https://russpass.ru")
+
+    browser.element(".OnboardingModal_onboarding-window__content__aId7h").should(be.visible)
+    browser.element(".OnboardingModal_onboarding-window__content__aId7h .button").click()
+
+    browser.element(".localSwitcher_wrapper__VC_5Q").click()
+    browser.element("//div[normalize-space(.)='English']").click()
+    browser.element("//div[normalize-space(.)='USD']").click()
+    browser.element("//button[normalize-space(.)='Сохранить']").click()
+
+    browser.element(".MainSearch_title__yuGrp").should(have.text("Plan Perfect Trip With")).should(
+        have.text("RUSSPASS"))
+
+    browser.quit()
